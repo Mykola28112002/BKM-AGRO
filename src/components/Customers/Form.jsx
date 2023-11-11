11e57699fb150b538055991f042f9c394c1eb264
Микола Кондратюк <95100219+Mykola28112002@users.noreply.github.com>
2023-11-11 16:41:16 +0200

    1

--- src/components/Customers/Form.jsx
+++ src/components/Customers/Form.jsx
@@ -2,47 +2,59 @@
 import React, { useState } from 'react';
 
 const ContactForm = () => {
-  const [formData, setFormData] = useState({
-    name: '',
-    phoneNumber: '',
-    message: '',
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
+    const [formData, setFormData] = useState({
+        name: '',
+        phoneNumber: '',
+        message: '',
     });
-  };
+    const [diasabled, setDiasabled] = useState(true);
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        if (formData.name.length <= 0) { 
+            setDiasabled(true)
+        } else {
+            setDiasabled(false)
+        }
+        setFormData({
+        ...formData,
+        [name]: value,
+        });
+    };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('Form data submitted:', formData);
-    setFormData({
-      name: '',
-      phoneNumber: '',
-      message: '',
-    });
-  };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+      
+        console.log('Form data submitted:', formData.name);
+        console.log('Form data submitted:', formData.phoneNumber);
+        if (formData.name.length <= 0) {
+            console.log('!!!!!!!!!!!!!!!!');
+        } else {
+            console.log('Form data submitted:', formData);
+            setFormData({
+                name: '',
+                phoneNumber: '',
+                message: '',
+            });
+        }
+    };
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        <input placeholder='Ваше ім’я' type="text" name="name" value={formData.name} onChange={handleChange} />
-      </label>
-      <br />
-      <label>
-        <input placeholder='Номер телефону' type="number" name="phoneNumber" value={formData.phoneNumber} onChange={handleChange} />
-      </label>
-      <br />
-      <label>
-        <textarea placeholder='Повідомлення ' name="message" value={formData.message} onChange={handleChange} />
-      </label>
-      <br />
-      <button type="submit">Замовити дзвінок</button>
-    </form>
-  );
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                <input placeholder='Ваше ім’я' type="text" name="name" value={formData.name} onChange={handleChange} />
+            </label>
+            <label>
+                <input placeholder='Номер телефону' type="number" name="phoneNumber" value={formData.phoneNumber} onChange={handleChange} />
+            </label>
+            <label>
+                <textarea placeholder='Повідомлення ' name="message" value={formData.message} onChange={handleChange} />
+            </label>
+            <button
+                disabled={diasabled}
+                style={{ opacity: diasabled ? 0.7 : 1 }}
+                type="submit">Замовити дзвінок</button>
+        </form>
+    );
 };
 
 export default ContactForm;
